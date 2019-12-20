dc65a0647273577953ffd98aba3644985a4ecc75
EvgenyKom <Jonney_komarov@mail.ru>
2019-12-20 22:45:55 +0700

    Добавлен модуль lambda.

---
+++ lambda/src/ru/evgkom/main/Main.java
@@ -0,0 +1,36 @@
+package ru.evgkom.main;
+
+import ru.evgkom.lambda.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Person> list = new ArrayList<>();
+
+        list.add(new Person("Иван", 20));
+        list.add(new Person("Сергей", 10));
+        list.add(new Person("Петр", 33));
+        list.add(new Person("Николай", 17));
+        list.add(new Person("Ирина", 50));
+        list.add(new Person("Ксения", 42));
+        list.add(new Person("Иван", 2));
+        list.add(new Person("Ирина", 21));
+
+        List<String> distinctNamesList = list.stream().map(Person::getName).distinct().collect(Collectors.toList());
+
+        String distinctNames = list.stream().map(Person::getName).distinct().collect(Collectors.joining(", ", "Имена: ", "."));
+        System.out.println(distinctNames);
+
+        List<Person> list1 = list.stream().filter(p -> p.getAge() < 18).collect(Collectors.toList());
+        list1.stream().mapToDouble(Person::getAge).average().ifPresent(avg -> System.out.println("Средний возраст людей младше 18 лет - " + avg));
+
+        Map<String, Double> avgAgeByNames = list.stream().collect(Collectors.groupingBy(Person::getName, Collectors.averagingDouble(Person::getAge)));
+        System.out.println(avgAgeByNames.toString());
+
+        list.stream().filter(p -> p.getAge() >= 20 && p.getAge() <= 40).sorted((p2, p1) -> p1.getAge() - p2.getAge()).map(Person::getName).forEach(s -> System.out.print(s + " "));
+    }
+}
